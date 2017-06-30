1d5797a1cad5572a0422205e68133901a6c35346
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-06-30 08:16:19 -0700

    add styling and add projects to the main page

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+
+
+  // setTimeout(function () {
+  //     $('typer').typer();
+  //     $.typer.options.typerInterval = 3000;
+  // }, 2000);
+
+  $('.nav').sticky();
+
+});
