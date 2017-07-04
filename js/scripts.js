181040b917d3b376fd3ebbc0172aafb92336792f
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-07-04 16:13:59 -0700

    Add logo icons to the about page

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,42 @@
-$( document ).ready(function() {
+$(document).ready(function() {
+
+	$('#toggle').click(function(event) {
+		event.preventDefault();
+      $(this).toggleClass('is-active');
+      $('#overlay-navbar').toggleClass('show');
+    });
 
 
-  // setTimeout(function () {
-  //     $('typer').typer();
-  //     $.typer.options.typerInterval = 3000;
-  // }, 2000);
-
-  $('.nav').sticky();
-
+	// navigation click actions
+	$('.scroll-link').on('click', function(event){
+		event.preventDefault();
+		var sectionID = $(this).attr("data-id");
+		scrollToID('#' + sectionID, 750);
+	});
+	// scroll to top action
+	$('.scroll-top').on('click', function(event) {
+		event.preventDefault();
+		$('html, body').animate({scrollTop:0}, 'slow');
+	});
+	// mobile nav toggle
+	$('#nav-toggle').on('click', function (event) {
+		event.preventDefault();
+		$('#main-nav').toggleClass("open");
+	});
 });
+// scroll function
+function scrollToID(id, speed){
+	var offSet = 50;
+	var targetOffset = $(id).offset().top - offSet;
+	var mainNav = $('#main-nav');
+	$('html,body').animate({scrollTop:targetOffset}, speed);
+	if (mainNav.hasClass("open")) {
+		mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+		mainNav.removeClass("open");
+	}
+}
+  if (typeof console === "undefined") {
+      console = {
+          log: function() { }
+      };
+  }
